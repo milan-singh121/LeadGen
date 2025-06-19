7e3251c7815bb70dd5545954790d50d12925ce41
Milan <milan@inhousen.com>
2025-06-19 17:38:45 +0530

    Updated the relative path

--- config/configuration_vars.py
+++ config/configuration_vars.py
@@ -17,22 +17,22 @@ class ConfigVars(metaclass=Singleton):
         configuration = Configuration()
         config = configuration.get_config()
 
-        # ===================== Mongo Credentials =====================#
-        self.mongo_uri = config["MONGO"]["MONGO_URI"]
+        # Mongo
+        self.mongo_uri = config["MONGO.MONGO_URI"]
 
-        # ===================== Secret Keys =====================#
-        self.rapid_api_key = config["KEY"]["RAPID_API_KEY"]
-        self.rapid_api_base_url = config["KEY"]["RAPID_API_BASE_URL"]
+        # Rapid API
+        self.rapid_api_key = config["KEY.RAPID_API_KEY"]
+        self.rapid_api_base_url = config["KEY.RAPID_API_BASE_URL"]
 
-        # ===================== GHL Keys =====================#
-        self.ghl_api_key = config["GHL"]["API_KEY"]
-        self.ghl_location_id = config["GHL"]["LOCATION_ID"]
+        # GHL
+        self.ghl_api_key = config["GHL.API_KEY"]
+        self.ghl_location_id = config["GHL.LOCATION_ID"]
 
-        # ===================== SNOV Keys =====================#
-        self.snov_user_id = config["KEY"]["SNOV_API_USERID"]
-        self.snov_secret_key = config["KEY"]["SNOV_API_SECRET"]
+        # SNOV
+        self.snov_user_id = config["KEY.SNOV_APIUSERID"]
+        self.snov_secret_key = config["KEY.SNOV_API_SECRET"]
 
-        # ===================== LLM Keys =====================#
-        self.aws_secret_key = config["LLM"]["AWS_SECRET_KEY"]
-        self.aws_access_key = config["LLM"]["AWS_ACCESS_KEY"]
-        self.aws_region = config["LLM"]["AWS_REGION"]
+        # LLM
+        self.aws_secret_key = config["LLM.AWS_SECRET_KEY"]
+        self.aws_access_key = config["LLM.AWS_ACCESS_KEY"]
+        self.aws_region = config["LLM.AWS_REGION"]
